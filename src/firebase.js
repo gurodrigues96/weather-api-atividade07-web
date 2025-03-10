5d4940b1b6fc42ea83e64164edd94258f351d1ad
Gustavo <104570961+gurodrigues96@users.noreply.github.com>
2025-03-10 10:58:18 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAZ6tOf3soQeb0FbtAXfyoiANFXOY5HkmU",
-  authDomain: "apirestdb07.firebaseapp.com",
-  projectId: "apirestdb07",
-  storageBucket: "apirestdb07.firebasestorage.app",
-  messagingSenderId: "232166344488",
-  appId: "1:232166344488:web:7337f277b22aa1a1a0f21d",
-  measurementId: "G-T8EE0BF8KC"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
-// Initialize Firebase
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Inicializa o Firestore
+const db = getFirestore(app);
+
+// Exporta o Firestore
+export { db };
